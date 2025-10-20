ca6c55cbfc9fb446fc5dd9cc652dddfe39ac7178
timeislife26 <jflemin26@gmail.com>
2025-10-20 22:48:25 +0100

    Added method to the Department name and to get all the users in a department

--- src/main/java/com/example/ticket_system/controllers/rest/DepartmentController.java
+++ src/main/java/com/example/ticket_system/controllers/rest/DepartmentController.java
@@ -1,23 +1,50 @@
 package com.example.ticket_system.controllers.rest;
 
 import com.example.ticket_system.DTO.DepartmentDTO;
+import com.example.ticket_system.DTO.UserDTO;
+import com.example.ticket_system.entities.Department;
 import com.example.ticket_system.mappers.DepartmentDTOMapper;
+import com.example.ticket_system.mappers.UserDTOMapper;
 import com.example.ticket_system.repositories.DepartmentRepository;
+import com.example.ticket_system.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class DepartmentController {
 
     private final DepartmentRepository departmentRepository;
+    private final MessageSource messageSource;
+    private final UserRepository userRepository;
 
-    @GetMapping("/departments")
+    @GetMapping("/department")
     public List<DepartmentDTO> getAll(){
         return departmentRepository.findAll()
                 .stream().map(DepartmentDTOMapper::toDto).toList();
+    }
+
+
+    @GetMapping("/department/{id}")
+    public String getDepartment(@PathVariable int id, Locale locale){
+        Optional<Department> department = departmentRepository.findById(id);
+        if (department.isPresent()){
+            return department.get().getDepartmentName();
+        }
+        return messageSource.getMessage("noDepartment", null, locale);
+    }
+
+    @GetMapping("/department/{id}/users")
+    public List<UserDTO> getDepartmentUsers(@PathVariable int id, Locale locale){
+        Optional<Department> department = departmentRepository.findById(id);
+            return userRepository.findByDepartment(department.get())
+                    .stream().map(UserDTOMapper::toDto).toList();
     }
 }
--- src/main/java/com/example/ticket_system/repositories/UserRepository.java
+++ src/main/java/com/example/ticket_system/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.ticket_system.repositories;
 
+import com.example.ticket_system.entities.Department;
 import com.example.ticket_system.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public List<User> findByDepartment(Department department);
 }
