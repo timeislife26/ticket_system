94020cd209af964974db96625f06549c6e1448d0
timeislife26 <jflemin26@gmail.com>
2025-10-22 23:05:45 +0100

    Added a function to get user by ID

--- src/main/java/com/example/ticket_system/controllers/rest/UserController.java
+++ src/main/java/com/example/ticket_system/controllers/rest/UserController.java
@@ -1,13 +1,16 @@
 package com.example.ticket_system.controllers.rest;
 
 import com.example.ticket_system.DTO.UserDTO;
+import com.example.ticket_system.entities.User;
 import com.example.ticket_system.mappers.UserDTOMapper;
 import com.example.ticket_system.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +23,17 @@ public class UserController {
     public List<UserDTO> getAllUsers(){
         return userRepository.findAll()
                 .stream().map(UserDTOMapper::toDto).toList();
+    }
+    @GetMapping("/users/{id}")
+    public UserDTO getUser(@PathVariable int id){
+        Optional<User> u = userRepository.findById(id);
+        User user = u.get();
+        return new UserDTO(
+                user.getUserId(),
+                user.getForename(),
+                user.getSurname(),
+                user.getEmail(),
+                user.getDepartment().getDepartmentName()
+        );
     }
 }
